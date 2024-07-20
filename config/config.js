a97b0731aa85fa972a8266184865fe1610ab8f5c
sijo jose k <sijojose099@gmail.com>
2024-07-20 20:44:15 +0530

    config file added

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const sessionSecret = 'mysecretsijojosek'
+const emailUser = 'sijojose099@gmail.com'
+const emailPassword = "sijojosek1234567890"
+
+module.exports = {
+    sessionSecret,
+    emailUser,
+    emailPassword
+}
