471d8f33f1737b4fe49e008acc4a25d848a0454a
Sijo Jose Kollannur <154460768+sijojosekollannur@users.noreply.github.com>
2024-07-21 21:55:45 +0530

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -21,7 +21,7 @@ module.exports = () => {
   passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://solehaven.live/users/auth/facebook/callback",
+    callbackURL: "https://solehaven.live/users/auth/facebook/callback",
     profileFields: ['id', 'emails', 'displayName'] // Request these fields
    },
 async (accessToken, refreshToken, profile, done) => {
@@ -43,7 +43,7 @@ async (accessToken, refreshToken, profile, done) => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://solehaven.live/users/auth/google/callback"
+    callbackURL: "https://solehaven.live/users/auth/google/callback"
   },
   async (token, tokenSecret, profile, done) => {
     try {
